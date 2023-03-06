b4bdef0273c7e46dc8c7a7319bc9f3e6825d4ea4
SalmaAhmedAli <salmahamzaahmed77@gmail.com>
2023-03-06 14:41:27 +0200

    View binding & view model implemented in RegisterActivity.kt

--- app/src/main/java/com/example/chatkotlinapp/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/chatkotlinapp/ui/register/RegisterActivity.kt
@@ -2,11 +2,17 @@ package com.example.chatkotlinapp.ui.register
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.chatkotlinapp.R
+import com.example.chatkotlinapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var dataBinding:ActivityRegisterBinding
+    lateinit var viewModel :RegisterViewMoel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        dataBinding= DataBindingUtil.setContentView(this,R.layout.activity_register)
+        viewModel=ViewModelProvider(this).get(RegisterViewMoel::class.java)
     }
 }
---
+++ app/src/main/java/com/example/chatkotlinapp/ui/register/RegisterViewMoel.kt
@@ -0,0 +1,6 @@
+package com.example.chatkotlinapp.ui.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewMoel:ViewModel() {
+}
